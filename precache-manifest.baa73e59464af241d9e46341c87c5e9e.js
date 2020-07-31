6d6565673c4532835f6a323f83ca4a9f6df25f47
erri120 <erri120@protonmail.com>
2020-07-31 08:37:02 +0000

    Update

---
+++ precache-manifest.baa73e59464af241d9e46341c87c5e9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6897d0483f0ab04ca5a1a21b9235c960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82cf5d1b9a27290b7f4d",
+    "url": "/static/css/main.24196c63.chunk.css"
+  },
+  {
+    "revision": "03ddf8e27315b7ba9abb",
+    "url": "/static/js/2.5a2231b2.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.5a2231b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82cf5d1b9a27290b7f4d",
+    "url": "/static/js/main.9420cb86.chunk.js"
+  },
+  {
+    "revision": "124d3105bda1387da73b",
+    "url": "/static/js/runtime-main.a1542236.js"
+  },
+  {
+    "revision": "de1ef58e8bcacb61d25b080244af7b83",
+    "url": "/static/media/wabbajack_transparent.de1ef58e.webp"
+  }
+]);
